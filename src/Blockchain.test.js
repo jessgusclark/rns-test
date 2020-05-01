83cba36b686acd27e17f8a1752355baea086f583
Jesse Clark <hello@developerjesse.com>
2020-05-01 18:03:29 +0300

    check incorrect balance.

--- src/Blockchain.test.js
+++ src/Blockchain.test.js
@@ -1,9 +1,16 @@
 import Web3 from 'web3';
 
 describe('testing connection to blockchain', () => {
+  const web3 = new Web3('http://0.0.0.0:8545');
+
   it('can get the latest block number', async () => {
-    const web3 = new Web3('http://0.0.0.0:8545');
     const currentBlock = await web3.eth.getBlock('latest');
     expect(currentBlock.number).toBe(0);
+  });
+
+  it('can get the balance of account 4', async () => {
+    const accounts = await web3.eth.getAccounts();
+    const balance = await web3.eth.getBalance(accounts[3]);
+    expect(parseInt(balance, 10)).toBe(100000000000000000001);
   });
 });
